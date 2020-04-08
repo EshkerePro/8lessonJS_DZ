679b5262d510d99081afe48fe438f72c967da01d
EshkerePro <mikola0672662963@gmail.com>
2020-04-08 14:56:11 +0300

    Homework js (DOM)

---
+++ jjs.js
@@ -0,0 +1,638 @@
+// // ZAVD 1
+//
+//   people =
+// {
+//     Petro : {
+//         car: true,
+//         wife: false,
+//         girl: true
+//     },
+//     Dog : {
+//         name: 'topick',
+//         old: 5,
+//         weight: 24
+//     },
+//     Car : {
+//         height: 169,
+//         speed: 220,
+//         color: 'black'
+//     },
+//     Mobile : {
+//         display: 5.5,
+//         android: 9,
+//         wi_fi: true
+//     },
+//     Skills : {
+//         java: true,
+//         javaScript: true,
+//         C: true,
+//         pyton: true
+//     },
+// };
+
+
+// // ZAVD 2
+//
+// let Programmer1 = {
+//        skill: ['JavaScript', 'Java', 'C++', 'C#', 'C', 'Pyton', 'Delphi', 'Php', 'Rubi', 'Pascal', 'Basic' ],
+//        name: 'EtotParen',
+//        old: 35,
+//        who: 'you'
+// };
+//
+// let Programmer2 = {
+//      skill: ['C++', 'JS', 'QA'],
+//     name: ['Entoni', 'Goshua'],
+//     girl: 'no'
+// };
+// let Programmer3 = {
+// skill: ['C', 'C#', 'C++'],
+//     old: 65,
+//     wi_fiAtHome: false
+// };
+//
+// let Programmer4 = {
+// skill: ['Delphi', 'Php', 'Rubi', 'Pascal', 'Basic'],
+//     status: true,
+//     children: {
+//     Olya : {
+//         old: 19,
+//          car: false
+//     },
+//         Petya: {
+//         old: 15,
+//             cat: 'Lubka'
+//         }
+//     },
+//     car: true,
+//     home: false
+// };
+// let Programmer5 = {
+// skill: false,
+//     status: 'bed',
+//     senyor: false,
+//     middle: false,
+//     junior: false
+// };
+
+
+// // ZAVD 3
+//
+// for (let i  in people) {
+//     console.log(i);
+// }
+//
+// for (let i in Programmer1) {
+//     console.log(i);
+// }
+//
+// for (let i in Programmer2) {
+//     console.log(i);
+// }
+//
+// for (let i in Programmer3) {
+//     console.log(i);
+// }
+//
+// for (let i in Programmer4) {
+//     console.log(i);
+// }
+//
+// for (let i in Programmer5) {
+//     console.log(i);
+// }
+
+
+// // ZAVD 4
+//
+// console.log(Object.keys(people));
+// console.log(Object.keys(Programmer1));
+// console.log(Object.keys(Programmer2));
+// console.log(Object.keys(Programmer3));
+// console.log(Object.keys(Programmer4));
+// console.log(Object.keys(Programmer5));
+
+
+// // ZAVD 5
+//
+// let cars = [
+//     bugatti = {
+//         model: 'veyron',
+//         yearBuy: 2018,
+//         strong: 2200,
+//         color: 'black'
+//     },
+//     lamba = {
+//         model: 'lomber',
+//         yearBuy: 2016,
+//         strong: 1200,
+//         color: 'red'
+//     },
+//     mers = {
+//         model: 'bens',
+//         yearBuy: 2018,
+//         strong: 900,
+//         color: 'white'
+//     },
+//     bmw = {
+//         model: 'X5',
+//         yearBuy: 2019,
+//         strong: 600,
+//         color: 'black'
+//     },
+//     audi = {
+//         model: 'spl',
+//         yearBuy: 2005,
+//         strong: 450,
+//         color: 'silver'
+//     },
+//     kia = {
+//         model: 'Q5',
+//         yearBuy: 2015,
+//         strong: 400,
+//         color: 'green'
+//     },
+//     toyota = {
+//         model: 'corola',
+//         yearBuy: 2012,
+//         strong: 400,
+//         color: 'black'
+//     },
+//     porsh = {
+//         model: 'horse',
+//         yearBuy: 1992,
+//         strong: 600,
+//         color: 'red'
+//     },
+//     nissan = {
+//         model: 'almera',
+//         yearBuy: 2005,
+//         strong: 280,
+//         color: 'black'
+//     },
+//     renault = {
+//         model: 'kengoo',
+//         yearBuy: 2003,
+//         strong: 190,
+//         color: 'white'
+//     }
+// ];
+
+
+// // ZAVD 6
+//
+// let cities = [
+//     Kulikiv = {
+//        people: 17000,
+//        contry: 'Ukranian',
+//         location: 'west'
+//     },
+//     Lviv = {
+//         people: 900000,
+//         contry: 'Ukranian',
+//         location: 'west'
+//     },
+//     Kiev = {
+//         people: 1700000,
+//         contry: 'Ukranian',
+//         location: 'center'
+//     },
+//     New_York = {
+//         people: 4000000,
+//         contry: 'USA',
+//         location: 'east'
+//     },
+//
+// ];
+
+
+// // ZAVD 7
+//
+// let cars1 = [
+//     bugatti = {
+//     model: 'veyron',
+//         yearStart: 2020,
+//         power: 2200,
+//         color:'black',
+//         driver: {
+//         name: 'Mykola',
+//             old: 19,
+//             sex: 'man',
+//             expiriense: 1
+//         },
+//     },
+//     bmw = {
+//         model: 'X6',
+//         yearStart: 2019,
+//         power: 800,
+//         color:'black',
+//         driver: {
+//             name: 'Andre',
+//             old: 26,
+//             sex: 'man',
+//             expiriense: 4
+//         },
+//     },
+//     porshe = {
+//         model: 'panomera',
+//         yearStart: 2018,
+//         power: 780,
+//         color:'red',
+//         driver: {
+//             name: 'Petro',
+//             old: 33,
+//             sex: 'man',
+//             expiriense: 12
+//         },
+//     },
+// ];
+
+
+// // ZAVD 8
+//
+// let i = 0;
+//
+// while (i < cars.length) {
+//     console.log(cars[i]);
+//     i++;
+// }
+// i = 0;
+// console.log('\n ');
+// while (i < cities.length) {
+// console.log(cities[i]);
+// i++;
+// }
+// i = 0;
+// console.log('\n');
+// while (i<cars1.length) {
+//     console.log(cars1[i]);
+//     i++;
+// }
+
+
+// // ZAVD 9
+//
+// for (let i = 0; i < cars.length; i++) {
+//     console.log(cars[i]);
+// }
+// console.log('\n');
+//
+// for (let i = 0; i < cars1.length; i ++) {
+//     console.log(cars1[i]);
+// }
+// console.log('\n');
+//
+//
+// for (let i = 0; i < cities.length; i++) {
+//     console.log(cities[i]);
+// }
+// console.log('\n');
+
+
+// // ZAVD 10
+//
+//
+//  for (let i of cars) {
+//      console.log(i);
+//  }
+//  console.log('\n');
+//
+//  for(let i of cars1) {
+//      console.log(i);
+//  }
+// console.log('\n');
+//  for (let i of cars) {
+//      console.log(i);
+//  }
+
+
+// // ZAVD 11
+//
+// let Jpeople = JSON.stringify(people);
+// console.log(Jpeople);
+
+
+// // ZAVD 12
+//
+// let NotJpeaple = JSON.parse(Jpeople);
+// console.log(NotJpeaple);
+
+
+// // ZAVD 13
+//
+// let Jcars;
+// for (let i = 0; i < cars.length; i++) {
+//     Jcars = JSON.stringify(cars[i]);
+// }
+// console.log(Jcars);
+
+
+// // ZAVD 14
+// let Jcities;
+// let i = 0;
+// while (i<cities.length) {
+//     Jcities = JSON.stringify(cities[i]);
+//     i++;
+// }
+// console.log(Jcities);
+
+
+// // ZAVD 15
+//
+// let cars1Copy = [];
+//
+// for(let i = 0; i < cars1.length; i++) {
+//     let Jcars1 = JSON.stringify(cars1[i]);
+//     cars1Copy.push(cars1[i]);
+// }
+// console.log(cars1Copy);
+
+
+// // ZAVD 16
+//
+// let user =  [
+//     user1 = {
+//     name: 'petya',
+//         old: 20,
+//     skill: ['java', 'js']
+//     },
+//     user2 = {
+//     name: 'baskov',
+//         old: 44,
+//         skill: ['js', 'C++']
+//     },
+//     user3 = {
+//     name: 'Vinnik',
+//         old: 29,
+//         skill: ['pyton', 'C', 'js']
+//     }
+// ];
+//
+// for (let i=0; i<user.length; i++) {
+//     console.log(user[i]);
+//     for (let a = 0; a < user[i].skill.length; a++) {
+//         console.log(user[i].skill[a]);
+//     }
+// }
+
+
+// // ZAVD 17
+//
+// let CopyUserSkill = [];
+// let user =  [
+//     user1 = {
+//         name: 'petya',
+//         old: 20,
+//         skill: ['java', 'js']
+//     },
+//     user2 = {
+//         name: 'baskov',
+//         old: 44,
+//         skill: ['js', 'C++']
+//     },
+//     user3 = {
+//         name: 'Vinnik',
+//         old: 29,
+//         skill: ['pyton', 'C', 'js']
+//     }
+// ];
+//
+// for (let i=0; i<user.length; i++) {
+//     console.log(user[i]);
+//     for (let a = 0; a < user[i].skill.length; a++) {
+//         CopyUserSkill.push(user[i].skill[a]);
+//     }
+// }
+// console.log(CopyUserSkill);
+
+
+// ZAVD 18
+//
+// let users = [
+//     {
+//         name: 'vasya',
+//         age: 31,
+//         status: false,
+//         skills: ['java', 'js']
+//     },
+//     {
+//         name: 'petya',
+//         age: 30,
+//         status: true,
+//         skills: ['java', 'js', 'html']
+//     },
+//     {
+//         name: 'kolya',
+//         age: 29,
+//         status: true,
+//         skills: ['mysql', ',mongo']
+//     },
+//     {
+//         name: 'olya',
+//         age: 28,
+//         status: false,
+//         skills: ['java', 'js']
+//     },
+//     {
+//         name: 'max',
+//         age: 30,
+//         status: true,
+//         skills: ['mysql', 'mongo']
+//     }
+//     ];
+// for (let i = 0; i<users.length; i++) {
+//     console.log(users[i]);
+//     for (let a = 0; a < users[i].skills[a]; a++) {
+//         console.log(users[i].skills[a]);
+//     }
+// }
+
+
+// // ZAVD 19
+//
+// let CopyUsersAge = [];
+// for (let i = 0; i < users.length; i++) {
+//     CopyUsersAge[i] = users[i].age;
+//         console.log(CopyUsersAge[i]);
+// }
+
+
+
+// // ZAVD 20
+//
+// let rul = document.getElementById('rul');
+// let rull = document.getElementsByClassName('rul2');
+// let h3 = document.getElementsByTagName('h3');
+
+
+
+// // ZAVD 21
+//
+// rul.innerText = 'smvvfdthhdfhyttttttttttttttlmkfmvk';
+// rull[0].innerText = 'dlfmvkkdmkfvmdr;lvd ';
+// h3[0].innerText = 'vmv';
+
+
+
+// // ZAVD 22
+//
+// rul.style.width = '400px';
+// rul.style.height = '200px';
+//
+// rull[0].style.height = '150px';
+//
+// h3[0].style.height = '300px';
+
+
+// // ZAVD 23
+//
+// let tab = document.createElement("table");
+// let  tr = document.createElement("tr");
+// let t1 = document.createElement('td');
+// let t2 = document.createElement('td');
+// let t3 = document.createElement('td');
+//
+// tab.appendChild(tr);
+// tr.appendChild(t1);
+// tr.appendChild(t2);
+// tr.appendChild(t3);
+//
+// document.body.appendChild(tab);
+
+
+
+// //  ZAVD 24
+//
+// let  tab = document.createElement("table");
+//
+//
+//     for (let a1 = 0; a1<3; a1++) {
+//         let tr = document.createElement('tr');
+//         tab.appendChild(tr);
+//         for (let i = 0; i < 10; i++) {
+//             let td = document.createElement('td');
+//             td.innerText = 'hello';
+//             tr.appendChild(td);
+//         }
+//     }
+//
+//     document.body.appendChild(tab);
+
+
+// // ZAVD 25
+//
+// let tab = document.createElement('table');
+//
+// for (let i = 0; i < 5; i++) {
+//     let tr = document.createElement('tr');
+//     tab.appendChild(tr);
+//     for (let a = 0; a < 10; a++) {
+//         let td = document.createElement('td');
+//         td.innerText = 'yess';
+//         tr.appendChild(td);
+//     }
+// }
+//
+// document.body.appendChild(tab);
+
+
+// //  ZAVD 26
+//
+// let m = +prompt('введіть кількість рядків');
+// let n = +prompt('введіть кількість комірок');
+//
+// let tab = document.createElement('table');
+//
+// for (let i = 0; i < m; i++) {
+//     let tr = document.createElement('tr');
+//     tab.appendChild(tr);
+//     for (let a = 0; a < n; a++) {
+//         let td = document.createElement('td');
+//         td.innerText = 'Okten Web';
+//         tr.appendChild(td);
+//     }
+// }
+//
+// document.body.appendChild(tab);
+
+
+// ZAVD 27 - В АРХІВІ
+
+
+ //          CLASSWORk
+
+// // ZAVD 28
+//
+// let idshka = document.getElementById('content');
+// let h2 = document.getElementsByTagName('h2');
+// let ul = document.createElement('ul');
+// for (let i of h2) {
+//     let li = document.createElement('li');
+//     li.innerText = i.innerText;
+//     ul.appendChild(li);
+// }
+// console.log(ul);
+// idshka.appendChild(ul);
+
+
+// // ZAVD 29
+//
+// let rules = [
+//     {
+//         title: 'Первое правило Бойцовского клуба.',
+//         body: 'Никому не рассказывать о Бойцовском клубе.'
+//     },
+//     {
+//         title: 'Второе правило Бойцовского клуба.',
+//         body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+//     },
+//     {
+//         title: 'Третье правило Бойцовского клуба.',
+//         body: 'В схватке участвуют только двое.'
+//     },
+//     {
+//         title: 'Четвертое правило Бойцовского клуба.',
+//         body: 'Не более одного поединка за один раз.'
+//     },
+//     {
+//         title: 'Пятое правило Бойцовского клуба.',
+//         body: 'Бойцы сражаются без обуви и голые по пояс.'
+//     },
+//     {
+//         title: 'Шестое правило Бойцовского клуба.',
+//         body: 'Поединок продолжается столько, сколько потребуется.'
+//     },
+//     {
+//         title: 'Седьмое правило Бойцовского клуба.',
+//         body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+//     },
+//     {
+//         title: 'Восьмое и последнее правило Бойцовского клуба.',
+//         body: 'Новичок обязан принять бой.'
+//     },
+//
+// ];
+//
+// let wrap =  document.createElement('div');
+// wrap.id = 'wrap';
+//
+// for (let i = 0; i < rules.length; i++) {
+//     let div = document.createElement('div');
+//     div.className = 'rule rules' + i;
+//     let h2 = document.createElement('h2');
+//     let p = document.createElement('p');
+//
+//     h2.innerText = rules[i].title;
+//     p.innerText = rules[i].body;
+//
+//     div.appendChild(h2);
+//     div.appendChild(p);
+//
+//     wrap.appendChild(div);
+// }
+//
+// document.body.appendChild(wrap);
+
+
+
+
---
+++ zavd 27/jss.js
@@ -0,0 +1,24 @@
+//  ZAVD 27
+// --Завантажити з мережі будь-який шаблон сайту. Підключити до нього свій скріпт-файл. У файлі прописати наступні доступи та дії
+// - знайти всі елементі, які мають class
+// - знайти всі параграфи ,та змінити текст на hello oktenweb!
+//     - знайти всі div та змінити ім колір на червоний
+//
+// let tagArr = document.getElementsByTagName('*');
+//
+// for (let tag of tagArr ) {
+//     if (tag.getAttribute('class')) {
+//         console.log(tag);
+//     }
+// }
+//
+// let p = document.getElementsByTagName('p');
+// for (let i of p) {
+//         i.innerText = 'hello oktenweb!';
+// }
+//
+// let divka = document.getElementsByTagName('div');
+//
+// for (let i of divka) {
+//     i.style.backgroundColor = 'red';
+// }
